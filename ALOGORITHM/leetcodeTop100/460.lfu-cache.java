14ff2b9459693a8292a3016dbd0d15d13d6c4187
xinlinyang2333 <1239821403@qq.com>
2024-06-05 16:44:20 +0800

    LRU ALOGORITHM Update

---
+++ ALOGORITHM/leetcodeTop100/460.lfu-cache.java
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode id=460 lang=java
+ *
+ * [460] LFU Cache
+ */
+
+// @lc code=start
+class LFUCache {
+
+    public LFUCache(int capacity) {
+        
+    }
+    
+    public int get(int key) {
+        
+    }
+    
+    public void put(int key, int value) {
+        
+    }
+}
+
+/**
+ * Your LFUCache object will be instantiated and called as such:
+ * LFUCache obj = new LFUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
+// @lc code=end
+
